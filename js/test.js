3e5ac1d80a6c7b5b099ac1b3e056b27824b69de1
MariusKM <m.morhard@hotmail.de>
2022-03-13 23:01:39 +0100

    Fixed Import Errors

--- js/test.js
+++ js/test.js
@@ -1,19 +1,18 @@
-import * as THREE from 'https://unpkg.com/three@0.138.3/build/three.module.js';
+import * as THREE from 'https://cdn.skypack.dev/three@0.127.0/build/three.module.js';
 
 
-import Stats from 'https://unpkg.com/browse/three@0.138.3/examples/jsm/libs/stats.module.js';
+import Stats from 'https://cdn.skypack.dev/three@0.127.0/examples/jsm/libs/stats.module.js';
 
-import { GUI } from 'https://unpkg.com/browse/three@0.138.3/examples/jsm/libs/lil-gui.module.min.js';
+import { GUI } from 'https://cdn.skypack.dev/three@0.127.0/examples/jsm/libs/dat.gui.module.js';
 
-import { OrbitControls } from 'https://unpkg.com/browse/three@0.138.3/examples/jsm/controls/OrbitControls.js';
-import { GLTFLoader } from 'https://unpkg.com/browse/three@0.138.3/examples/jsm/loaders/GLTFLoader.js';
-import { EffectComposer } from 'https://unpkg.com/browse/three@0.138.3/examples/jsm/postprocessing/EffectComposer.js';
-import { RenderPass } from 'https://unpkg.com/browse/three@0.138.3/examples/jsm/postprocessing/RenderPass.js';
-import { ShaderPass } from 'https://unpkg.com/browse/three@0.138.3/examples/jsm/postprocessing/ShaderPass.js';
-import { EXRLoader } from 'https://unpkg.com/browse/three@0.138.3/examples/jsm/loaders/EXRLoader.js';
-import { ClearPass } from 'https://unpkg.com/browse/three@0.138.3/examples/jsm/postprocessing/ClearPass.js';
-import { CopyShader } from 'https://unpkg.com/browse/three@0.138.3/examples/jsm/shaders/CopyShader.js';
-
+import { OrbitControls } from 'https://cdn.skypack.dev/three@0.127.0/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.127.0/examples/jsm/loaders/GLTFLoader.js';
+import { EffectComposer } from 'https://cdn.skypack.dev/three@0.127.0/examples/jsm/postprocessing/EffectComposer.js';
+import { RenderPass } from 'https://cdn.skypack.dev/three@0.127.0/examples/jsm/postprocessing/RenderPass.js';
+import { ShaderPass } from 'https://cdn.skypack.dev/three@0.127.0/examples/jsm/postprocessing/ShaderPass.js';
+import { EXRLoader } from 'https://cdn.skypack.dev/three@0.127.0/examples/jsm/loaders/EXRLoader.js';
+import { ClearPass } from 'https://cdn.skypack.dev/three@0.127.0/examples/jsm/postprocessing/ClearPass.js';
+import { CopyShader } from 'https://cdn.skypack.dev/three@0.127.0/examples/jsm/shaders/CopyShader.js';
 
 
 let scene = new THREE.Scene();
